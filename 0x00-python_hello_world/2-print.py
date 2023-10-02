ced28c433f220aeeb86db2985ccab08d1a23ca7c
franklinAnozie <cfanozie@gmail.com>
2023-10-02 11:45:56 +0100

    attempted more tasks

---
+++ 0x00-python_hello_world/2-print.py
@@ -0,0 +1,2 @@
+#!/usr/bin/env python3
+print(f'"Programming is like building a multilingual puzzle')
---
+++ 0x00-python_hello_world/5-print_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+str = "Holberton School"
+print(f"{str * 3}", end="")
+print("")
+print(str[:9])
