c42bfc8acb18004bdea69e8eebb5e799a0e0daf0
franklinAnozie <cfanozie@gmail.com>
2023-10-06 01:59:36 +0100

    fixed errors

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,13 +3,15 @@ if __name__ == "__main__":
     from sys import argv
 
 length = len(argv)
+len_gth = length - 1
 
-if length == 1:
-    print(f"{length-1} arguments.")
-elif length == 2:
-    print(f"{length-1} argument:")
+if len_gth == 1:
+    print("{} argument:".format(len_gth))
+elif len_gth == 0:
+    print("{} arguments.".format(len_gth))
 else:
-    print(f"{length-1} arguments:")
+    print("{} arguments:".format(len_gth))
 
-for i in range(length):
-    print(f"{i}: {argv[i]}")
+if len_gth > 0:
+    for i in range(1, length):
+        print("{}: {}".format(i, argv[i]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     sum = 0
     for a in range(1, len(argv)):
         sum += int(argv[a])
-    print(sum)
+    print("{}".format(sum))
