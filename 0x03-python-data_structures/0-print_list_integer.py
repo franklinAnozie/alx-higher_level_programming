9544d931b9845cd35625d936f0f46105a97ba54b
franklinAnozie <cfanozie@gmail.com>
2023-10-09 00:06:13 +0100

    fixed README.md

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-# function that prints all integers of a list.
 def print_list_integer(my_list=[]):
     for i in my_list:
-        print(f"{i}", end="")
-    
-    print("")
+        print("{:.d}".format(i))
 
-print_list_integer([1, 2, 3, 4, 5])
+
+if __name__ == "__main__":
+    print_list_integer()
