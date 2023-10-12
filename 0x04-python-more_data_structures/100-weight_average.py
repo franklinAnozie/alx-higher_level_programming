b2ccc76c2e70446c19745600dfb2b8ccc644ccab
franklinAnozie <cfanozie@gmail.com>
2023-10-12 06:09:12 +0100

    made fix

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,9 +3,10 @@ def weight_average(my_list=[]):
     average = 0
     score = 0
     weight = 0
-    if my_list is not None:
+    if my_list is not None and len(my_list) > 0:
         for i in range(len(my_list)):
             score = score + my_list[i][0] * my_list[i][1]
             weight += my_list[i][1]
-        average = score / weight
+        if weight != 0:
+            average = score / weight
     return average
