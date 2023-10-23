db28625b43702a287a0e01238e95260f3a0b0c54
franklinAnozie <cfanozie@gmail.com>
2023-10-23 23:42:35 +0100

    fixed both

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    retVal = 0
     try:
         for i in range(x):
             print("{}".format(my_list[i]), end="")
+            retVal += 1
         print()
-        i += 1
     except IndexError:
         print()
-    return i
+    return (retVal)
 
 
 if __name__ == "__main__":
