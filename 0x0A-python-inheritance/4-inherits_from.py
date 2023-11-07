e94e4490ca5db122e614f7d84f58f5ea304ad92c
franklinAnozie <cfanozie@gmail.com>
2023-11-07 03:21:50 +0100

    corrections

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""This module contains a function that returns True
-if the object is an instance of a class that inherited
-(directly or indirectly)
-from the specified class ; otherwise False."""
+"""
+Module containing function inherits_from
+checks that an object and a class are the same or share same superclasss
+"""
 
 
 def inherits_from(obj, a_class):
