ffb8508d2136fac447db46c2b9e7aa59c2c6b401
franklinAnozie <cfanozie@gmail.com>
2024-02-19 00:54:01 +0100

    fixed some more errors

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -11,11 +11,16 @@ Base = declarative_base()
 
 
 class City(Base):
+    """
+    City class from Base
 
-    def __init(self):
-        pass
+    Attributes:
+        id: id that will be asigned
+        name: name of the city
+        state: state city is located
+    """
 
     __tablename__ = 'cities'
-    id = Column(Integer, nullable=False, primary_key=True)
+    id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,8 +9,15 @@ Base = declarative_base()
 
 
 class State(Base):
-    def __init(self):
-        pass
+    """
+    State class sup calss from Base
+    ORM first try :D
+
+    Attributes:
+        id: id that will be asigned from the sql
+        name: name of the state
+    """
+
     __tablename__ = 'states'
-    id = Column(Integer, nullable=False, primary_key=True)
+    id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
