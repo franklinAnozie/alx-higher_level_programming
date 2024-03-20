3b4816a3ec476e7222d9d8d06032df87959de1fb
franklinAnozie <cfanozie@gmail.com>
2024-03-20 14:51:27 +0100

    attempted task two

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Script that lists all priviledges of MySQL users
+
+CREATE USER IF NOT EXISTS `user_0d_1`@`localhost` IDENTIFIED BY `user_0d_1_pwd`;
+GRANT ALL PRIVILEDGES ON *.* TO `user_0d_1`@`localhost`;
