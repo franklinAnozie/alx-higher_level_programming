d96a1edf1f29b72a74c29d561e8866ce2b93a564
franklinAnozie <cfanozie@gmail.com>
2024-03-21 17:53:33 +0100

    conclude mandatory

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- list all genres
+
+SELECT tv_genres.name, COUNT(tv_show_genres.genre_id)
+AS num_of_shows
+FROM tv_genres INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY num_of_shows DESC;
