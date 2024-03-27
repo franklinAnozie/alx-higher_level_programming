6c7eb4f8492c5989101dd568059e2b47c04d7278
franklinAnozie <cfanozie@gmail.com>
2024-03-27 05:49:31 +0100

    fixed some issues

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 
 const readme = require('fs');
 const args = process.argv[2];
--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 
 const request = require('request');
 const URL = 'https://swapi-api.alx-tools.com/api/films/';
